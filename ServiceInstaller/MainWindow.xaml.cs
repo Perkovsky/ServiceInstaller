7119f045aaca2596a6f6d7a99fecec74c4a40ff0
admin <admin@ELCAPITANE>
2017-07-14 16:17:14 +0300

    Prerelease #1

--- ServiceInstaller/MainWindow.xaml.cs
+++ ServiceInstaller/MainWindow.xaml.cs
@@ -13,6 +13,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.ServiceProcess;
+using System.Configuration.Install;
+using System.Diagnostics;
+using System.IO;
+using System.Management;
 
 namespace ServiceInstaller
 {
@@ -21,7 +26,30 @@ namespace ServiceInstaller
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly string SERVICE_NAME_UNDEFINED = "Service name: undefined";
         private OpenFileDialog openFileDialog = new OpenFileDialog();
+        private string serviceName;
+
+        private string GetServiceName(string fileNameService)
+        {
+            string result = null;
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_Service");
+            ManagementObjectCollection collection = searcher.Get();
+
+            foreach (ManagementObject obj in collection)
+            {
+                string name = obj["Name"] as string;
+                string pathName = obj["PathName"] as string;
+                if (pathName != null && pathName.Contains(fileNameService))
+                {
+                    result = name;
+                    break;
+                }
+            }
+            tbServiceName.Text = (result == null) ? SERVICE_NAME_UNDEFINED : string.Format("Service name: {0}", result);
+
+            return result;
+        }
 
         public MainWindow()
         {
@@ -37,6 +65,82 @@ namespace ServiceInstaller
             if (openFileDialog.ShowDialog() == true)
             {
                 tbFileServiceName.Text = openFileDialog.SafeFileName;
+                serviceName = GetServiceName(openFileDialog.FileName);
+            }
+            else
+            {
+                tbFileServiceName.Text = "";
+                serviceName = "";
+            }
+        }
+
+        private void btnInstall_Click(object sender, RoutedEventArgs e)
+        {
+            if (tbFileServiceName.Text.Length == 0)
+            {
+                MessageBox.Show("Select file NT-Service!");
+                return;
+            }
+
+            try
+            {
+                ManagedInstallerClass.InstallHelper(new[] { openFileDialog.FileName });
+                serviceName = GetServiceName(openFileDialog.FileName);
+                MessageBox.Show(string.Format("Service \"{0}\" has been installed!", openFileDialog.SafeFileName));
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.ToString());
+            }
+        }
+
+        private void btnUninstall_Click(object sender, RoutedEventArgs e)
+        {
+            if (tbFileServiceName.Text.Length == 0)
+            {
+                MessageBox.Show("Select file NT-Service!");
+                return;
+            }
+
+            try
+            {
+                ManagedInstallerClass.InstallHelper(new[] { @"/u", openFileDialog.FileName });
+                serviceName = GetServiceName(openFileDialog.FileName);
+                MessageBox.Show(string.Format("Service \"{0}\" has been uninstalled!", openFileDialog.SafeFileName));
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.ToString());
+            }
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            ServiceController controller;
+            try
+            {
+                controller = new ServiceController { ServiceName = serviceName };
+                controller.Start();
+                MessageBox.Show(string.Format("Service \"{0}\" Started", serviceName));
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.ToString());
+            }
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            ServiceController controller;
+            try
+            {
+                controller = new ServiceController { ServiceName = serviceName };
+                controller.Stop();
+                MessageBox.Show(string.Format("Service \"{0}\" Stopped", serviceName));
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.ToString());
             }
         }
     }
